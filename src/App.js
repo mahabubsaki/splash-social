785c7b1df9a66b4b05c1ab3a21f56cd8ee0e83d0
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-08-08 13:08:44 +0600

    initialized git repo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="App root">
+
+      </div>
+      <div className="invalid-device h-screen w-full justify-center items-center text-red-500 text-3xl">
+        <h1 className="text-center">Please use smaller device to visit this site</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
